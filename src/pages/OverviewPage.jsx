7e968138d50b91904aeee46c300975208c7c4870
saisathwik22 <reddysaisathwik022@gmail.com>
2024-08-21 17:04:56 +0530

    OverView Page completed Stage: 58:00

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -2,6 +2,9 @@ import Header from "../components/common/Header";
 import { motion } from "framer-motion";
 import { StatCard } from "../components/common/StatCard";
 import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
+import SalesOverviewChart from "../components/overview/SalesOverviewChart";
+import CategoryDistributionChart from "../components/overview/CategoryDistributionChart";
+import SalesChannelChart from "../components/overview/SalesChannelChart";
 
 const OverviewPage = () => {
   return (
@@ -41,6 +44,14 @@ const OverviewPage = () => {
             color='#10B981'
           />
         </motion.div>
+
+        {/* Charts */}
+
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+          <SalesOverviewChart />
+          <CategoryDistributionChart />
+          <SalesChannelChart />
+        </div>
       </main>
     </div>
   );
